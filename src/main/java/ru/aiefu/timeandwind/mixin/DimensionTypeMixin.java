f17f6a7e5489d67b684dd918332c8b43098fc9e7
CmdrJane <48736985+CmdrJane@users.noreply.github.com>
2021-05-01 17:41:37 +0300

    -

--- src/main/java/ru/aiefu/timeandwind/mixin/DimensionTypeMixin.java
+++ src/main/java/ru/aiefu/timeandwind/mixin/DimensionTypeMixin.java
@@ -31,12 +31,14 @@ public class DimensionTypeMixin implements IDimType {
 	@Inject(method = "getMoonPhase", at =@At("HEAD"), cancellable = true)
 	private void patchMoonPhase(long time, CallbackInfoReturnable<Integer> cir){
 		cir.setReturnValue((int)(time / this.cycleTime % 8L + 8L) % 8);
+		return;
 	}
 
 	@Inject(method = "getSkyAngle", at = @At("HEAD"), cancellable = true)
 	private void calculateCelestialAngle(long time, CallbackInfoReturnable<Float> cir){
 		if(!this.fixedTime.isPresent()){
 			cir.setReturnValue((float) syncTime(dayDuration, nightDuration, time));
+			return;
 		}
 	}
 
@@ -83,5 +85,4 @@ public class DimensionTypeMixin implements IDimType {
 	public float getPrevAngle() {
 		return (float) this.prevAngle;
 	}
-
 }

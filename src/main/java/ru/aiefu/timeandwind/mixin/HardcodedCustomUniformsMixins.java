fde6421a2ad38f19a0368ad416acbcdafc0bd717
CmdrJane <commanderjanedoe@gmail.com>
2021-09-08 00:40:03 +0300

    possible shaders fix(I hope)

--- src/main/java/ru/aiefu/timeandwind/mixin/HardcodedCustomUniformsMixins.java
+++ src/main/java/ru/aiefu/timeandwind/mixin/HardcodedCustomUniformsMixins.java
@@ -1,22 +1,44 @@
 package ru.aiefu.timeandwind.mixin;
 
+import net.coderbot.iris.mixin.DimensionTypeAccessor;
 import net.coderbot.iris.uniforms.HardcodedCustomUniforms;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ru.aiefu.timeandwind.IDimType;
 
 @Pseudo
 @Mixin(HardcodedCustomUniforms.class)
 public class HardcodedCustomUniformsMixins {
-    @ModifyConstant(method = "getTimeAngle()F", constant = @Constant(floatValue = 24000.0F), remap = false)
-    private static float patchTAWTA(float f){
-        return (float) ((IDimType) MinecraftClient.getInstance().world.getDimension()).getCycleDuration();
+    @Inject(method = "getTimeAngle", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void calcTAWAngle(CallbackInfoReturnable<Float> cir){
+
+        World world = MinecraftClient.getInstance().world;
+        long timeOfDay = world.getTimeOfDay();
+        long dayTime = ((DimensionTypeAccessor)world.getDimension()).getFixedTime().orElse(timeOfDay);
+        IDimType iDimType = ((IDimType) world.getDimension());
+        long dayD = iDimType.getDayDuration();
+        long nightD = iDimType.getNightDuration();
+        double mod = dayTime % (dayD + nightD);
+        double d;
+        double f;
+        if(mod > dayD){
+            mod -= dayD;
+            f = 0.5D / nightD;
+            d = f * mod + 0.5D;
+        }
+        else {
+            f = 0.5D / dayD;
+            d = f * mod;
+        }
+        cir.setReturnValue((float) d);
+        cir.cancel();
     }
-    @ModifyConstant(method = "getWorldDayTime()I", constant = @Constant(longValue = 24000L), remap = false)
-    private static long patchTAWWDT(long l){
-        return ((IDimType) MinecraftClient.getInstance().world.getDimension()).getCycleDuration();
-    }
+
 }

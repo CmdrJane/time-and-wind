7b091b31596d0529cc5fbd1e255280055ba8d9dc
CmdrJane <commanderjanedoe@gmail.com>
2023-03-26 14:53:36 +0300

    1.4.6 backport

--- src/main/java/ru/aiefu/timeandwindct/tickers/Ticker.java
+++
@@ -1,7 +0,0 @@
-package ru.aiefu.timeandwindct.tickers;
-
-import ru.aiefu.timeandwindct.ITimeOperations;
-
-public interface Ticker {
-    void tick(ITimeOperations world, boolean nskip, int acceleration);
-}
